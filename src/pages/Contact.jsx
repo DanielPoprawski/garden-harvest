9a9e47c3ace76b56f59dc8fd2c2856245d511f7c
Daniel Poprawski <danielbpoprawski@gmail.com>
2024-12-30 20:34:52 -0500

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,6 @@ import { Table } from "react-bootstrap";
 export default function Contact() {
         return (
                 <div className="hero">
-                        // TODO: Fix this ugly ass contact page
                         <h1> Contact us today: </h1>
                         <h4>Our address: 3825 W 11th Ave, Ste 1, Eugene, OR 97402</h4>
                         <h4>Call us at (541) 653 - 8459</h4>
